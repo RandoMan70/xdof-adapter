9abd19642c46705dfcdf062af8126996ece7cfb4
Sergey Kovalev <xray@randoman.ru>
2021-03-01 19:43:29 +0700

    Initial

---
+++ main.py
@@ -0,0 +1,13 @@
+import pyserial
+import socket
+
+
+def push_state(left_front: int, right_front: int, rear: int, turn: int, wind: int)
+
+def main():
+    pass
+    # pyserial.
+
+
+if __name__ == '__main__':
+    main()
